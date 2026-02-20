e94d14177c89b0d311f3f2298dea53f7454ca0b8
Bossman64532 <obrown3@uw.edu>
2026-02-20 14:20:11 -0800

    server runs and communicates with the client

---
+++ Server/gridappsd-2030_5/test_client.py
@@ -0,0 +1,34 @@
+import ieee_2030_5.client.client 
+
+from ieee_2030_5.client.client import IEEE2030_5_Client
+
+
+# Certificate paths
+CA   = "/home/mess/tls/certs/ca.crt"
+CERT = "/home/mess/tls/certs/03.pem"        # dev1 certificate
+KEY  = "/home/mess/tls/private/dev1.pem"   # dev1 private key
+
+# Create client
+client = IEEE2030_5_Client(
+   cafile=CA,
+   server_hostname="192.168.92.130",
+   keyfile=KEY,
+   certfile=CERT,
+   server_ssl_port=8443,
+   debug=True
+)
+
+print("\n=== DEVICE CAPABILITY ===")
+dcap = client.device_capability("/dcap")
+print(dcap)
+
+print("\n=== TIME ===")
+print(client.time())
+
+print("\n=== END DEVICES ===")
+edev_list = client.end_devices()
+for ed in edev_list.EndDevice:
+   print("sFDI:", ed.sFDI, "href:", ed.href)
+
+client.disconnect()
+
